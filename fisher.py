4d2c5df5ae69dff3f6fb65aabf78e8ee209cd43b
louis <llsmingyi@163.com>
2019-04-12 21:48:01 +0800

    init repo

---
+++ fisher.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import DEBUG
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/hello/')
+def hello():
+    return "hello"
+
+# app.add_url_rule('/hello/', view_func=hello)
+
+app.run(host='0.0.0.0', debug=app.config['DEBUG'])
+
